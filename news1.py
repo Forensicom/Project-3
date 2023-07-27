c1b3ab455ea40b6aa096eab85e305fa07526c540
teamuoft <81271728+AnnaKass1@users.noreply.github.com>
2023-07-26 20:57:25 -0400

    Update news1.py

--- news1.py
+++ news1.py
@@ -2,14 +2,13 @@ import feedparser
 import webbrowser
 import streamlit as st
 import pandas as pd
-feed = feedparser.parse("https://feeds.bloomberg.com/markets/news.rss")
+feed = feedparser.parse("https://cointelegraph.com/rss")
 feed_title = feed["feed"]["title"]
 feed_entried = feed.entries
 for entry in feed.entries:
     article_title = entry.title
     article_link = entry.link
     
-print ("{}[{}]".format(article_title, article_link))
 data = {}
 for entry in feed.entries:
     data.setdefault("title",[])
